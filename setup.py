166e52bd5c7bf8984d7c414e113049b633e9ed06
twilson <trumanmwilson@gmail.com>
2018-05-20 16:09:56 -0400

    Adding a setup script.  I want to test the repo on my RPi.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+if __name__ == '__main__':
+
+    setup(
+        name='rpi_bluetooth_listener',
+        version='0.0',
+        packages=['rpi_bluetooth_listener'],
+    )
